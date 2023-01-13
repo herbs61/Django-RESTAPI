5dac08676cdec2105d490cc25291e99019084fe5
Herbert Owusu <77298594+herbs61@users.noreply.github.com>
2023-01-13 20:37:20 +0000

    Update

--- movies/views.py
+++ movies/views.py
@@ -8,4 +8,12 @@ class MovieViewSet(viewsets.ModelViewSet):
     queryset = Moviedata.objects.all()
     serializer_class = MovieSerializer
     
+    
+class ActionViewSet(viewsets.ModelViewSet):
+    queryset = Moviedata.objects.filter(typ='action')
+    serializer_class = MovieSerializer
 
+
+class ComedyViewSet(viewsets.ModelViewSet):
+    queryset = Moviedata.objects.filter(typ='comedy')
+    serializer_class = MovieSerializer
